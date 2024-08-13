978c9ff076d538f14adc3a3f2dc2be90f2be2aea
Lada <lada11lada@gmail.com>
2024-08-14 00:50:00 +0200

    styling for Contact and ContactList, removing contacts

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,22 @@
 import PropTypes from 'prop-types';
+import css from './Contact.module.css';
+import { FaPhone } from 'react-icons/fa';
+import { IoPerson } from 'react-icons/io5';
+import { MdOutlineDeleteForever } from 'react-icons/md';
+import classNames from 'classnames';
 
 function Contact({ contact, deleteContact }) {
   return (<>
-    <li>{contact.name}: {contact.phoneNumber}</li>
+    <li className={css.contactCard}>
+      <div className={css.itemsContainer}>
+        <div className={css.item}><FaPhone /><p>{contact.name}</p></div>
+        <div className={css.item}><IoPerson /><p>{contact.phoneNumber}</p></div>
+      </div>
+      <button className={classNames(css.item, css.button)} type="button" onClick={() => deleteContact(contact.id)}>
+        <MdOutlineDeleteForever />
+        Delete
+      </button>
+    </li>
   </>);
 }
 
