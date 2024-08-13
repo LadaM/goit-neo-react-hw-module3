a7c6e68e25075c1751104d6b325cfcfa17c297e1
Lada <lada11lada@gmail.com>
2024-08-14 00:55:41 +0200

    saving contacts to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,11 @@ import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import ContactList from './components/ContactList/ContactList.jsx';
 
-const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', phoneNumber: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', phoneNumber: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', phoneNumber: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', phoneNumber: '227-91-26' },
-];
-
-
 function App() {
-  const [contacts, setContacts] = useState(initialContacts);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    return savedContacts ? JSON.parse(savedContacts) : [];
+  });
 
   const addContact = (contact) => {
     setContacts((prevContacts) => {
@@ -28,7 +23,7 @@ function App() {
   };
 
   useEffect(() => {
-    console.log(contacts);
+    localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
   return (
